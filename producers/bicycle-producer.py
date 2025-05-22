fe22dc640980f9d19a26cfa44838ce8218d74380
JUNGSOO8 <soo8600@gmail.com>
2025-05-22 16:29:34 +0900

    test

--- producers/bicycle-producer.py
+++ producers/bicycle-producer.py
@@ -14,11 +14,7 @@ class BicycleProducer():
     def __init__(self, topic):
         self.topic = topic
         self.conf = {'bootstrap.servers': BROKER_LST,
-                     'compression.type':'lz4',
-                     'enable.idempotence': 'true',
-                     'max.in.flight.requests.per.connection': '5',
-                     'acks': 'all'
-        }
+                     'compression.type':'lz4'}
         self.producer = Producer(self.conf)
         self._set_logger()
 
